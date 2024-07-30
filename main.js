b2436e20bce457fd1a75072c40ba80aca4ca171e
ahmed magdy <ahmedmagdy492@yahoo.com>
2024-07-30 04:15:22 +0300

    changed images to webp

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 window.addEventListener('load', function() {
     (function () {
-        const URL = "https://ahmedmagdy492.github.io/data/projects.json";
+        const URL = "https://raw.githubusercontent.com/ahmedmagdy492/ahmedmagdy492.github.io/main/data/projects.json";
         const contentContainer = document.querySelector('#projects-content');
     
         fetch(URL, {
@@ -8,8 +8,61 @@ window.addEventListener('load', function() {
         })
         .then(data => data.json())
         .then(data => {
-            console.log(data);
+            const projects = [...data.projects];
+
+            contentContainer.innerHTML = '';
+
+            projects.forEach(project => {
+                const divContainer = document.createElement('div');
+                divContainer.className = 'card';
+                const divImgContainer = document.createElement('div');
+                divContainer.appendChild(divImgContainer);
+                const img = document.createElement('img');
+                img.className = 'card-img-top';
+                img.src = project.posterUrl;
+                img.alt = project.altText;
+                divImgContainer.appendChild(img);
+
+                const descDiv = document.createElement('div');
+                divContainer.appendChild(descDiv);
+                const cardBodyDiv = document.createElement('div');
+                cardBodyDiv.className = 'card-body';
+                descDiv.appendChild(cardBodyDiv);
+                const cardTitle = document.createElement('h5');
+                cardTitle.innerText = project.name;
+                cardTitle.className = 'card-title';
+                cardBodyDiv.appendChild(cardTitle);
+                const cardText = document.createElement('p');
+                cardText.className = 'card-text';
+                cardText.innerHTML = project.description;
+
+                if(project.links) {
+                    const links = [...project.links];
+
+                    links.forEach(link => {
+                        const a = document.createElement('a');
+                        a.href = link.url;
+                        a.style.marginBottom = '5px';
+                        if(link.target)
+                            a.target = link.target;
+                        if(link.img) {
+                            const linkImg = document.createElement('img');
+                            linkImg.src = link.img;
+                            a.appendChild(linkImg);
+                        }
+                        else {
+                            a.className = 'btn btn-primary';
+                            a.innerText = link.text;
+                        }
+                        cardBodyDiv.appendChild(a);
+                    });
+                }
+
+                cardBodyDiv.appendChild(cardText);
+
+                contentContainer.appendChild(divContainer);
+            });
         })
-        .catch(err => contentContainer.innerHTML = "<h1 style='color: red;' class='text-center'>Failed to reterive projects data</h1>");
+        .catch(err => contentContainer.innerHTML = `<h5 style='color: red;' class='text-center'>Failed to reterive projects data: ${err}</h5>`);
     })();
 });
